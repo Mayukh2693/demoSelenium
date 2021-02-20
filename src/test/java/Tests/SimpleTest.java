7685842f5f1f52f66ce1c3e921c97972815a5a10
Mayukh Nath <mnath077@apac.comcast.com>
2021-02-20 09:52:02 +0530

    Checking docker

--- src/test/java/Tests/SimpleTest.java
+++ src/test/java/Tests/SimpleTest.java
@@ -7,8 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -22,14 +21,15 @@ public class SimpleTest {
 	   @BeforeTest
 	   public void setup() throws MalformedURLException{
 	       //use Chrome Driver
-		   DesiredCapabilities dcap = DesiredCapabilities.chrome();
-		   dcap.setCapability("headless", true);
-//		   String driverpath = "C:/Users/mnath077/eclipse-workspace/Selenium/drivers/chromedriver.exe";
-//		   System.setProperty("webdriver.chrome.driver",driverpath);
+		   //DesiredCapabilities dcap = DesiredCapabilities.chrome();
+		   //dcap.setCapability("headless", true);
+		   String driverpath = "C:/Users/mnath077/eclipse-workspace/Selenium/drivers/chromedriver.exe";
+		   System.setProperty("webdriver.chrome.driver",driverpath);
 //	       driver = new ChromeDriver();
 		   // Run on docker container
-		   URL gamelan = new URL("http://localhost:4444/wd/hub");
-	       driver = new RemoteWebDriver(gamelan, dcap);
+		   //URL gamelan = new URL("http://localhost:4444/wd/hub");
+		   ChromeOptions options = new ChromeOptions().setHeadless(true);
+	       driver = new ChromeDriver(options);
 	       driver.manage().window().maximize();
 	       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	   }
